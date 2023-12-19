2f76477fee2847ce71a2bb64e41b3d57d24b5dfc
Priyank222 <priyankpansuriya123@gmail.com>
2023-12-19 16:40:56 +0530

    add dark theme and update portfolio

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 // Some code thanks to @chrisgannon
+var tog = 1;
+$('#fordark').css('background-color', '#292929')
+$('.bold').css('color', '#dcdcdc')
+$('.profile').css('background', '#292929')
+$('.profile').css('color', '#dcdcdc')
+$('.profile h1').css('color', '#dcdcdc')
+$('.profile .list-titles').css('color', '#dcdcdc')
+function change_theme() {
+  // var theme = document.getElementById("theme");
+  // console.log(theme.href);
+  // if(theme.href == "http://localhost/my_portfoliyo/style.css") {
+  //   document.getElementById("theme").href = "style2.css"
+  // } else {
+  //   document.getElementById("theme").href = "style.css"
+  // }
+  if(tog == 0) {
+    $('#fordark').css('background-color', '#292929')
+    $('.bold').css('color', '#dcdcdc')
+    $('.profile').css('background', '#292929')
+    $('.profile').css('color', '#dcdcdc')
+    $('.profile h1').css('color', '#dcdcdc')
+    $('.profile .list-titles').css('color', '#dcdcdc')
+    tog = 1;
+  } else {
+    $('#fordark').css('background-color', '#fff')
+    $('.bold').css('color', '#4a4e51')
+    $('.profile').css('background', '#fff')
+    $('.profile').css('color', '#4a4e51')
+    $('.profile h1').css('color', '#4a4e51')
+    $('.profile .list-titles').css('color', '#4a4e51')
+    tog = 0;
+  }
+
+}
 
 var select = function(s) {
     return document.querySelector(s);
